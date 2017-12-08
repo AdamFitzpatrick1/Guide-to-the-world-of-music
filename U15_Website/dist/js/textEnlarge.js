135f97e206658d8bd5ebc97fee47185ddf632f95
AdamFitzpatrick1 <31987448+AdamFitzpatrick1@users.noreply.github.com>
2017-12-08 09:05:08 +0000

    Add files via upload

---
+++ U15_Website/dist/js/textEnlarge.js
@@ -0,0 +1,7 @@
+function textEnlarge() {
+  document.getElementById("Intro").style.fontSize = "xx-large";
+}
+
+function textReduce() {
+  document.getElementById("Intro").style.fontSize = "15pt";
+}
